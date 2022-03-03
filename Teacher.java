054252b0ec94f7d4f30d2edc52c14cbb8f7d7113
ZTZWILL <92398270+ZTZWILL@users.noreply.github.com>
2022-03-03 16:45:00 +0000

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,6 +1,10 @@
 public class Teacher {
 
     private String name;
+    pirvate String major;
+    private int teaching_year;
+    private int course_num;  //this represent how many course currently 
+    private int student_rating;
 
     public Teacher(String personName) {
         this.name = personName;
@@ -9,4 +13,20 @@ public class Teacher {
     public String getName() {
         return this.name;
     }
+    public int calScore(){
+        int experience_score=0;
+        int stress_score=0;
+        int satisfication_score=0;
+        if (teaching_year>10)
+            experience_score=10;
+        else
+            experience_score=teaching_year;
+        if (course_num>=3)
+            stress_score=0;
+        else 
+            stress_score=(3-course_num)*3;
+        stress_score=student_rating;
+        int total_score=0.5*stress_score+0.2*experience_score+0.3*satisfication;
+        return total_score;
+
 }
