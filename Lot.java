dafa4507f155dec52019bb1f3f133ae0c610eeb8
Haotian-Shi-cyber <62081228+Haotian-Shi-cyber@users.noreply.github.com>
2022-03-04 20:45:06 +0000

    Update Lot.java

--- Lot.java
+++ Lot.java
@@ -1,9 +1,8 @@
 
-
 public class Lot{
     
     private Teacher[] list;
-    private nteachers;
+    private int nteachers;
     private static int max_teachers=100;
     public Lot() {
         list  = new Teacher[max_teachers];
@@ -13,15 +12,20 @@ public class Lot{
     public int getTeacherNum(){
         return nteachers;
     }
+    
     public void addTeacher(Teacher teacher){
-        list[nteacher++]=teacher;
+        list[nteachers++]=teacher;
     }
+    
     public Teacher find(String personName) {
         for(int j = 0; j < nteachers; j++)
             if(list[j].getName().equals(personName))
                 return list[j];
         return null;
     }
-
+    
+    public Teacher[] getTeacherList() {
+    	return list;
+    }
 
 }
