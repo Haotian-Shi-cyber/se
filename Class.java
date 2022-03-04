2ed3b85d0eeae6d5314644c24b92828e09f5ada5
Luoxuan Peng <2668528p@student.gla.ac.uk>
2022-03-04 00:50:00 +0000

    Update Class.java

--- Class.java
+++ Class.java
@@ -1,21 +1,22 @@
-public class Class{
+import java.io.*;
 
+
+public class Class {
+    
     private String name;
     private String college;
 
     public Class(String className, String classCollege) {
-        this.name = claasName;
+        this.name = className;
         this.college = classCollege;
     }
-    public void print(PrintStream cs, Class class)	{
-		cs.print(String.format("name: %s", class.name));	
-		cs.print(String.format("college: %s", class.college));
-	}
+
+
     public String getName() {
-		return this.name;
+        return this.name;
 	}
     public String getCollege() {
-		return this.college;
+        return this.college;
 	}
 
 }
