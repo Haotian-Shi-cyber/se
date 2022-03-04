8253c6cbe9bf65e7e9c4b2f2d24c497e2650a9d8
Luoxuan Peng <2668528p@student.gla.ac.uk>
2022-03-04 00:43:45 +0000

    Update Loc.java

--- Loc.java
+++ Loc.java
@@ -1,13 +1,28 @@
 /* list of classes  */
+import java.io.*;
 
 public class Loc {
 
     private Class[] list;
-    private int max;
+    private int i,max;
 
     public Loc() {
+        max = 10;
         list = new Class[max];
+        i = 0;
     }
+    public void add(Class classes)	{
+		list[i++] = classes;
+	}
 
+    public void remove(Class classes) {
+        int	j;
+        for (j = 0; j < i; j++)
+            if (list[j] == classes)
+                break;
+        i--;
+        for (; j < i; j++)
+            list[j] = list[j+1];
+    }
 
 }
