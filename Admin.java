ce827a824d3cf69f89282dc8425961cfc51a91ea
Haotian-Shi-cyber <62081228+Haotian-Shi-cyber@users.noreply.github.com>
2022-03-05 21:38:21 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -3,6 +3,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
+import java.util.HashMap;
 
 
 public class Admin{
@@ -10,6 +11,9 @@ public class Admin{
 	private JSONObject jsonObject;
 	private JSONArray teacherArray;
 	private JSONArray classArray;
+	private HashMap<String,String> map;
+	private Lot teacherList;
+	private Loc classList;
 	
 	public void read_file() throws IOException, ParseException {
 		JSONParser parser = new JSONParser();
@@ -19,17 +23,17 @@ public class Admin{
 		jsonObject = (JSONObject) parser.parse(reader);
 	}
 
-	public void add_staff() {
+	public void add_staff() throws Lopx {
 		Lot teacherList = new Lot();
 		
 		teacherArray = (JSONArray) jsonObject.get("teacher lists"); // get teacher array 
 		
 		for (int i = 0; i < teacherArray.size(); i++) {
 			JSONObject tmp = (JSONObject) teacherArray.get(i);
-			Teacher teacher = new Teacher(tmp.get("name"), tmp.get("major"));
-			teacherList.add(teacher);
-		}
-		
+			Teacher teacher = new Teacher((String)tmp.get("name"),(String)tmp.get("major"),(int)tmp.get("teaching_year"), 
+										(int)tmp.get("course_on_charge"),(int)tmp.get("student_rating"));
+			teacherList.addTeacher(teacher);
+		}		
 	}
 	
 	public void add_class() {
@@ -39,46 +43,62 @@ public class Admin{
 		
 		for (int i = 0; i < classArray.size(); i++) {
 			JSONObject tmp = (JSONObject) classArray.get(i);
-			Class classobj = new Class(tmp.get("classname"), tmp.get("requirements"));
-			classList.add(teacher);
+			Class classobj = new Class((String)tmp.get("classname"), (String)tmp.get("college"));
+			classList.add(classobj);
 		}
 	}
 	
 	
-	public String find_suitable_staff(Class thisclass,Lot lot) {// this function return the target teacher name
-		int list_size=lot.getTeacherNum;
-		String target_teacher;
-		int index;
-		int highest_score;
-		for(int i=0,index=i;i<list_size;i++){
-			Teacher t=lot[i];
+	private String find_suitable_staff(Class thisclass, Lot lot) {// this function return the target teacher name
+		int list_size=lot.getTeacherNum();
+		int index1 = 0;
+		int highest_score = 0;
+		
+		Teacher[] list = lot.getTeacherList();
+		
+		for(int i=0;i<list_size;i++){
+			Teacher t=list[i];
 			if(t.getMajor()!= thisclass.getCollege()){
 				continue;
 			}
 			int score=t.calScore();
 			if (score > highest_score){
 				highest_score= score;
-				index=i;
+				index1=i;
 			}
 		}
-		return lot[index].getName;
+		return list[index1].getName();
 		
 	}
 	
-	public void create_file() {
+	private void match() {
+		String teachername;
+		HashMap<String,String> map = new HashMap<String, String>();
 		
+		for (int i = 0; i < classList.getSize(); i++) {
+			String classname;
+			
+			teachername = find_suitable_staff(classList.get(i), teacherList);
+			classname = classList.get(i).getName();
+			map.put(classname, teachername);
+		}
 	}
 	
-	public void main(String[] args) throws IOException, ParseException{
+	public void create_file() throws IOException {
+		JSONObject result = new JSONObject(map);
+		FileWriter file = new FileWriter("./result.json");
+        file.write(result.toJSONString());
+        file.close();
+	}
+	
+	
+	public void main(String[] args) throws IOException, ParseException, Lopx{
 		
 		read_file();
 		add_staff();
 		add_class();
-		find_suitable_staff();
-		create_file();
-		
-		
+		match();
+		create_file();		
 	}
-	
-	
+		
 }
