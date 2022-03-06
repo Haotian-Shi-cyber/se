bcf4bf8d89d034d499dca2959c2e923b6925d3c6
Haotian-Shi-cyber <62081228+Haotian-Shi-cyber@users.noreply.github.com>
2022-03-06 22:28:17 +0000

    Update Main1.java

--- Main1.java
+++ Main1.java
@@ -1,5 +1,5 @@
 import java.io.IOException;
-import java.util.Date;
+
 
 import org.json.simple.*;
 import org.json.simple.parser.ParseException;
@@ -8,21 +8,25 @@ public class Main1 {
 
 	public static void main(String[] args) throws Lopx, IOException, ParseException	{
 		
-		FileProcessor file = new FileProcessor("./teaching_requirements_and_teachers_list.json");
-		file.read_file();
-		JSONArray teacherArray = file.get_teachers_array();
-		JSONArray classArray = file.get_classes_array();
+		FileProcessor file = new FileProcessor("./teaching_requirements_and_teachers_list.json"); // read JSON file
+		file.read_file(); //parse it
+		JSONArray teacherArray = file.get_teachers_array(); // get teaching requirements from file
+		JSONArray classArray = file.get_classes_array(); // get teacher lists from file
+		
+		System.out.println("print teacherArray: " + teacherArray);
+		System.out.println("print classArray: " + classArray);
 		
 		class_director Alice = new class_director(); // Alice is the class director
-		Alice.add_all_staff(teacherArray);
-		Alice.add_all_class(classArray);
+		Alice.add_all_staff(teacherArray); // add all staff to list
+		Alice.add_all_class(classArray); // add all class to list
 		
-		Admin Bob = new Admin();
-		Bob.match_all(Alice.get_classList(), Alice.get_teacherList());
-		Bob.create_file();
-		/*
-		 to be added, set training
-		 */
+		Admin Bob = new Admin(); // Bob is the administrator
+		Bob.match_all(Alice.get_classList(), Alice.get_teacherList()); // find all suitable staff for all class
+		Bob.create_file(); // create a new file containing teacher versus class
+		Bob.set_training(Alice.get_teacherList()); // set training for staff
+		
+		System.out.println("print created file: " + Bob.get_map());
+		System.out.println("print training list: " + Bob.get_trainingList());
 		System.out.println("good night");
 	}
 	
