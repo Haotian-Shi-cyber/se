9a5cce203b7552b34b85e8815704d06ccc4d9519
Luoxuan Peng <2668528p@student.gla.ac.uk>
2022-03-10 02:52:41 +0000

    add file to src

--- src/assess2/Main1.java
+++ src/assess2/Main1.java
@@ -10,7 +10,7 @@ public class Main1 {
 
 	public static void main(String[] args) throws Lopx, IOException, ParseException	{
 		
-		FileProcessor file = new FileProcessor("teaching_requirements_and_teachers_list.json"); // read JSON file
+		FileProcessor file = new FileProcessor("assess2/teaching_requirements_and_teachers_list.json"); // read JSON file
 		file.read_file(); //parse it
 		JSONArray teacherArray = file.get_teachers_array(); // get teaching requirements from file
 		JSONArray classArray = file.get_classes_array(); // get teacher lists from file
@@ -22,7 +22,7 @@ public class Main1 {
 		Alice.add_all_staff(teacherArray); // add all staff to list
 		Alice.add_all_class(classArray); // add all class to list
 		
-		Admin Bob = new Admin("admin","major"); // Bob is the administrator
+		Admin Bob = new Admin("assess2.admin","major"); // Bob is the administrator
 		Bob.match_all(Alice.get_classList(), Alice.get_teacherList()); // find all suitable staff for all class
 		Bob.create_file(); // create a new file containing teacher versus class
 		Bob.set_training(Alice.get_teacherList()); // set training for staff
